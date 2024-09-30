2ed5e8f9bab381cd06c06bbbdaf46d9a979252cb
nksound <NKSound@mail.ru>
2024-09-30 13:22:54 +0300

    "Многопроцессное программирование"

--- module_10_5.py
+++ module_10_5.py
@@ -1,14 +1,30 @@
+from multiprocessing import Pool
+from datetime import datetime
+
+
 def read_info(name):
     all_data = []
-    with open(name, 'r', encoding='utf-8') as file:
-        for file.readline in file:
-            all_data.append(file.readline)
-    with open('file.txt', 'w', encoding='utf-8') as info:
-        info.write(str(all_data))
-
+    with open(name) as file:
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            all_data.append(line.strip())
 
 
 if __name__ == '__main__':
     filenames = [f'./Files/file {number}.txt' for number in range(1, 5)]
-    # for i in filenames:
-    read_info(filenames[1])
+
+#  Линейный вызов
+    start = datetime.now()
+    for filename in filenames:
+        read_info(filename)
+    end = datetime.now() - start
+    print(f'{end} (линейный)')
+
+# Многопроцессный вызов
+    start = datetime.now()
+    with Pool() as pool:
+        pool.map(read_info, filenames)
+    end = datetime.now() - start
+    print(f'{end} (многопроцессный)')
